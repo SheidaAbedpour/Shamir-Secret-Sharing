4091f8cdddd4d654db7a64bf3b112a0929dfb81b
Sheida Abedpour <sheida.abedpour2002@gmail.com>
2024-07-01 15:07:36 +0330

    n shares generated

--- main.py
+++ main.py
@@ -17,6 +17,16 @@ class SSS:
         if not isprime(p):
             raise ValueError("p must be prime")
         self.coeffs = [self.s] + [random.randint(1, self.p-1) for _ in range(t-1)]
+        self.shares = self._generate_shares()
+        print(self.coeffs)
+        print(self.shares)
+
+    def _generate_shares(self):
+        shares = []
+        for i in range(1, self.n+1):
+            poly_i = sum([coff * (i ** power) for power, coff in enumerate(self.coeffs)]) % self.p
+            shares.append((i, poly_i))
+        return shares
 
 
 if __name__ == "__main__":
